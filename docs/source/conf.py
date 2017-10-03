9717c6272f9007e34c6e4ebac6844554fb5615a3
Ashok H <kohsah@users.noreply.github.com>
2017-10-03 22:44:44 +0530

    Updated author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Gawati Docs'
 copyright = u'2017, African Innovation Foundation'
-author = u'Ashok Hariharan'
+author = u'The Gawati Community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
